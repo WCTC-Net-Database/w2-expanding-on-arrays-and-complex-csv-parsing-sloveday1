794b3db393dd10296b8e80bfe90e4c3e68f40e49
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-09-06 23:31:09 -0500

    Completed Assignment for Module 1 + 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 class Program
@@ -42,37 +42,47 @@ class Program
 
     static void DisplayAllCharacters(string[] lines)
     {
+        
         // Skip the header row
         for (int i = 1; i < lines.Length; i++)
         {
             string line = lines[i];
 
             string name;
-            int commaIndex;
+            int commaIndex = line.IndexOf(",");
 
             // Check if the name is quoted
             if (line.StartsWith("\""))
             {
                 // TODO: Find the closing quote and the comma right after it
+                var firstQuote = line.IndexOf("\"");
+                line = line.Substring(firstQuote + 1);
+                var lastQuote = line.IndexOf("\"");
+                commaIndex = line.IndexOf(",", lastQuote);
                 // TODO: Remove quotes from the name if present and parse the name
-                // name = ...
+                name = line.Substring(0, commaIndex-1);
             }
             else
             {
                 // TODO: Name is not quoted, so store the name up to the first comma
-                // name =
+                name = line.Substring(0, commaIndex);
             }
 
             // TODO: Parse characterClass, level, hitPoints, and equipment
-            // string characterClass = ...
-            // int level = ...
-            // int hitPoints = ...
-
+            string[] fields = line.Split(',');
+            string characterClass = fields[fields.Length - 4];
+            
+            int level = Convert.ToInt32(fields[fields.Length - 3]); 
+            
+            int hitPoints = Convert.ToInt32(fields[fields.Length - 2]);
+            
+            
             // TODO: Parse equipment noting that it contains multiple items separated by '|'
-            // string[] equipment = ...
+            string[] equipment = fields[fields.Length - 1].Split("|");
+            
 
             // Display character information
-            // Console.WriteLine($"Name: {name}, Class: {characterClass}, Level: {level}, HP: {hitPoints}, Equipment: {string.Join(", ", equipment)}");
+            Console.WriteLine($"Name: {name}, Class: {characterClass}, Level: {level}, HP: {hitPoints}, Equipment: {string.Join(", ", equipment)}");
         }
     }
 
@@ -82,6 +92,25 @@ class Program
         // Prompt for character details (name, class, level, hit points, equipment)
         // DO NOT just ask the user to enter a new line of CSV data or enter the pipe-separated equipment string
         // Append the new character to the lines array
+        Console.WriteLine("What is the characters name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is the characters class:");
+        string characterClass = Console.ReadLine();
+        Console.WriteLine("What is the characters level:");
+        int level = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("What is the characters hit points:");
+        int hitPoints = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Equipment 1:");
+        string equipment1 = Console.ReadLine();
+        Console.WriteLine("Equipment 2:");
+        string equipment2 = Console.ReadLine();
+        Console.WriteLine("Equipment 3:");
+        string equipment3 = Console.ReadLine();
+
+        string character = $"{name},{characterClass},{level},{hitPoints},{equipment1}|{equipment2}|{equipment3}";
+
+        Array.Resize(ref lines, lines.Length + 1);
+        lines[lines.Length-1] = character;
     }
 
     static void LevelUpCharacter(string[] lines)
@@ -100,15 +129,18 @@ class Program
             {
 
                 // TODO: Split the rest of the fields locating the level field
-                // string[] fields = ...
-                // int level = ...
+                string[] fields = line.Split(',');
+                int level = Convert.ToInt32(fields[fields.Length - 3]);
+                string name = line.Substring(1, line.IndexOf(","));
 
                 // TODO: Level up the character
-                // level++;
-                // Console.WriteLine($"Character {name} leveled up to level {level}!");
+                level++;
+                Console.WriteLine($"Character {name} leveled up to level {level}!");
+                fields[fields.Length - 3] = Convert.ToString(level);
 
                 // TODO: Update the line with the new level
-                // lines[i] = ...
+                string character = String.Join(",", fields);
+                lines[i] = character;
                 break;
             }
         }
